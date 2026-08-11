2029be11b5509a527e096fc26a3aac4a2d5a260b
Testmaster01566 <amir.varisi@gmail.com>
2026-08-12 01:05:55 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAhVKiHVLSU1bFfoGE4Hm2Z0-UCcQD7YUI",
   authDomain: "surveycms.firebaseapp.com",
@@ -8,3 +9,7 @@ const firebaseConfig = {
   appId: "1:52495956563:web:47f5f2b48b2d18091724a6",
   measurementId: "G-6QGNCMKDG7"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
