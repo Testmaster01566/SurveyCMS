05b2baf7e76cf14eea738ef9ac59e32ca3dc74cd
Testmaster01566 <amir.varisi@gmail.com>
2026-08-12 00:58:34 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAhVKiHVLSU1bFfoGE4Hm2Z0-UCcQD7YUI",
   authDomain: "surveycms.firebaseapp.com",
   projectId: "surveycms",
   storageBucket: "surveycms.firebasestorage.app",
   messagingSenderId: "52495956563",
-  appId: "1:52495956563:web:47f5f2b48b2d18091724a6"
+  appId: "1:52495956563:web:47f5f2b48b2d18091724a6",
+  measurementId: "G-6QGNCMKDG7"
 };
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
